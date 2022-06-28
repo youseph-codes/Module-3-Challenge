7bb6cd0391fa6e501a4f3f9abe9638c25158e80c
Yousopp <yousephagueye@gmail.com>
2022-06-27 20:20:50 -0400

    wrote out array of symbols,numbers, and letters

--- Develop/script.js
+++ Develop/script.js
@@ -1,18 +1,62 @@
-// Assignment code here
-const lowercase = "abcdefghijklmnopqrstuvwxyz";
-const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const numbers = "0123456789";
-const symbols = "!#$%&'()*+,-./:;<=>?@[^_{|}~";
+// GLOBAL ASSIGNED VARIABLES
+let alphaCap = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+let alphaLow = 'acdefghijklnopqrstuvwxyz';
+let num = '0123456789';
+let specialChar = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+let allChar = '';
+let length = '';
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Get references to the #generate element 
+let generateBtn = document.querySelector("#generate");
+
+// GENERATE PASSWORD FUNCTION
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let passwordLengthPrompt = Number(prompt('Pick a password length from 8 to 128 characters.'));
+  //if password length is null, too small, or too large, alert user and re-run function
+  if (!passwordLengthPrompt || passwordLengthPrompt < 8 || passwordLengthPrompt > 128) {
+    alert('Please type a valid answer!');
+    writePassword();
+  }
+  //if length is sufficient, add number to the length variable
+  else {
+    length += passwordLengthPrompt;
+  }
 
-  passwordText.value = password;
+  //user prompts
+  let addLowercase = window.confirm('Click Okay if you would like to include lowercase characters.');
+  let addUppercase = window.confirm('Click Okay if you would like to include uppercase characters.');
+  let addNumbers = window.confirm('Click okay if you would like to include numbers.');
+  let addSpecialChar = window.confirm('Click okay if you would like to include special characters.');
+
+  //if user picks certain variables, add them to variable allChar. if allChar is null, return to writePassword function.
+  if (addLowercase) {
+    allChar += alphaLow;
+  }
+  if (addUppercase) {
+    allChar += alphaCap;
+  }
+  if (addNumbers) {
+    allChar += num;
+  }
+  if (addSpecialChar) {
+    allChar += specialChar
+  }
+  if (!allChar) {
+    alert('You must include at least one character type!');
+    writePassword();
+  }
+
+  //create new string to represent randomly selected characters using math.random method
+  let newPassword = '';
+  for (let i = 0; i < length; i++) {
+    newPassword += allChar.charAt(Math.floor(Math.random() * allChar.length));
+    console.log(newPassword);
+  }
+
+  //declare variable to select HTML DOM element and assign the value to be equal to newPassword variable
+  let password = document.querySelector("#password");
+  password.value = newPassword;
 
 }
 
